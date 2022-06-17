fa5ef3b1d35ca8b54f54382d37e16872f2922e9c
pensinegor@gmail.com <pensinegor@gmail.com>
2022-06-17 22:32:27 +0300

    Homework-3

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,13 @@
 import '../styles/main.scss';
 
-var buttonSend = document.getElementById('button-send');
-
-alert('Hello person. I want to ask you for some data and then display it');
-
-let onButtonClick = function() {
-
-    var firstName = document.getElementById('first-name').value;
-    var secondName = document.getElementById('second-name').value;
-    var dataBith = document.getElementById('data-of-bith').value;
-    var selectCountry = document.getElementById('select').value;
-    var genderChecked = function() {
-        var gender = document.getElementsByName('gender');
-        for (var i = 0; i < gender.length; i++) {
-            if (gender[i].checked) {
-                return gender[i].value;
-            }
-        }
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 != 0 && i % 15 != 0) {
+        console.log('Кратное 3: ' + i);
     }
-    var email = document.getElementById('email').value;
-
-    let isSure = confirm('Are you sure you entered everything correctly?');
-    if (isSure) {
-        var numberPhone = prompt('Almost forgot, enter phone number');
-        alert(` Your firstname: ${firstName}\n Your secondname: ${secondName}\n You was born: ${dataBith}\n Country: ${selectCountry}\n Your gender: ${genderChecked()}\n Your email: ${email}\n Your phone: ${numberPhone}`);
+    if (i % 3 != 0 && i % 5 == 0 && i % 15 != 0) {
+        console.log('Кратное 5: ' + i);
     }
-    else {
-        alert('Correct and resubmit the data');
+    if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+        console.log('Кратное 15: ' + i);
     }
 }
-
-buttonSend.addEventListener ('click', onButtonClick);
