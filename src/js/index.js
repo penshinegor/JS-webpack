d70197cc958783d8129895d44e2ce9346d182d9a
Egor Penshin <pensinegor@gmail.com>
2022-07-15 15:35:21 -0700

    Corrected HW3

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import '../styles/main.scss';
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 != 0 && i % 15 != 0) {
-        console.log('Кратное 3: ' + i);
+    if (i % 15 == 0) {
+        console.log('Кратное 15: ' + i);
     }
-    if (i % 3 != 0 && i % 5 == 0 && i % 15 != 0) {
+    else if (i % 5 == 0) {
         console.log('Кратное 5: ' + i);
     }
-    if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
-        console.log('Кратное 15: ' + i);
+    else if (i % 3 == 0) {
+        console.log('Кратное 3: ' + i);
     }
 }
