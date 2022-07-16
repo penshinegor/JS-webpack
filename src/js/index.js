ab94af160c78ab9ede69772b6dafbc01a7fb8e12
Egor Penshin <pensinegor@gmail.com>
2022-07-15 17:11:36 -0700

    Corrected HW4

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,7 @@ import '../styles/main.scss';
 
 //Function 1
 function EqualityTestFunction (a, b) {
-    if (a === b) {
-        return true;
-    }
-    return false;
+    return a === b;
 }
 console.log(EqualityTestFunction(2, 3));
 console.log(EqualityTestFunction(3, 3));
@@ -14,16 +11,11 @@ console.log(EqualityTestFunction('10', '10'));
 
 //Function 2
 function CheckDivisionFunction (x, y) {
-    if (x % y == 0) {
-        return x;
-    }
-    else {
-        while (true){
-            x++;
-            if (x % y == 0) {
-                return x;
-            }
+    while (true){
+        if (x % y == 0) {
+            return x;
         }
+        x++;
     }
 }
 console.log(CheckDivisionFunction(1, 23));
