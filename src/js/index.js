4a10b6fdc8f841a9206f500249ff76535e8489fa
Egor Penshin <pensinegor@gmail.com>
2022-07-15 17:26:40 -0700

    Corrected HW5

--- src/js/index.js
+++ src/js/index.js
@@ -5,41 +5,39 @@ const car = {
     typeBodyCar: 'Sedan',
     hasAutomaticTransmission: true,
     color: 'black',
-    maxSpeed: 275,
+    editionYear: 2004,
 
-    startDrive() { 
-        console.log('Our car started drive') 
+    showInfo() { 
+        return `Car ${this.name} has ${this.typeBodyCar} body type \nHaving an automatic transmission - ${this.hasAutomaticTransmission} \nColor: ${this.color} \nEdition year: ${this.editionYear}`;
     },
-    useBeep() { 
-        console.log('Beep-Beep') 
+    startEngine() { 
+        return `Engine is working. ${this.name} ready to go`;
     },
-    sportModeOn() { 
-        console.log('Sport mode on. Our car can drive faster') 
+    drive(speed) {
+        return `Car is driving with ${speed} speed`;
     }
 };
 
 console.log(car);
 
-function Car (name, typeBodyCar, hasAutomaticTransmission, color, maxSpeed) {
+function Car (name, typeBodyCar, hasAutomaticTransmission, color, editionYear) {
     this.name = name;
     this.typeBodyCar = typeBodyCar;
     this.hasAutomaticTransmission = hasAutomaticTransmission;
     this.color = color;
-    this.maxSpeed = maxSpeed;
+    this.editionYear = editionYear;
 
-    this.startDrive = function () {
-        console.log('Our car started drive')
+    this.showInfo = function () {
+        return `Car ${this.name} has ${this.typeBodyCar} body type \nHaving an automatic transmission - ${this.hasAutomaticTransmission} \nColor: ${this.color} \nEdition year: ${this.editionYear}`;
     };
-    this.useBeep = function () {
-        console.log('Beep-Beep') 
+    this.startEngine = function () {
+        return `Engine is working. ${this.name} ready to go`;
     };
-    this.sportModeOn = function () { 
-        console.log('Sport mode on. Our car can drive faster') 
+    this.drive = function (speed) { 
+        return `Car is driving with ${speed} speed`;
     };
 }
 
-const familyCar = new Car ('Volkswagen', 'Minivan', false, 'blue', 190);
+const familyCar = new Car ('Volkswagen', 'Minivan', false, 'blue', 1999);
 
-console.log(familyCar);
-
-
+console.log(familyCar); 
